e1a7c0f7d9cd46732296d5a96b0220a15e060026
Gabriel Santos <gabriel.santos@blitzpay.com.br>
2024-04-03 14:18:56 -0300

    remove start-at column from bet

---
+++ prisma/migrations/20240403171742_remove_start_at_from_bet/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `startAt` on the `Bet` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Bet" DROP COLUMN "startAt";

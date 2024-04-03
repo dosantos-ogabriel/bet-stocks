bd26008a17cbf7bd88c5ede702079a6ac821ad32
Gabriel Santos <gabriel.santos@blitzpay.com.br>
2024-04-03 15:56:28 -0300

    add bcrypt for password hashing

--- server/src/user.ts
+++ server/src/user.ts
@@ -1,9 +1,31 @@
+import { Prisma } from "@prisma/client";
+import bcrypt from "bcrypt";
+import prisma from "./db";
+
+const saltRounds = 10;
+
+interface LoginInput {
+  email: string;
+  password: string;
+}
+
 class User {
-  async login() {
-    interface LoginPayload {
-      name: string;
-      password: string;
-    }
+  async register(data: Prisma.UserCreateInput) {
+    const passwordHash = await bcrypt.hash(data.password, saltRounds);
+    data.password = passwordHash;
+
+    const user = await prisma.user.create({ data });
+
+    return user;
+  }
+  async login(data: LoginInput) {
+    const user = await prisma.user.findUniqueOrThrow({ where: data });
+
+    const isCorrectPassword = await bcrypt.compare(data.password, user.password);
+
+    if (isCorrectPassword) return user;
+
+    Promise.reject("Wrong password");
   }
 }
 

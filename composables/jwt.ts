147fb6284852ce4b27dd17395ed675bdc98711cc
Gabriel Santos <gabriel.santos@blitzpay.com.br>
2024-04-04 16:03:34 -0300

    create jwt composable

---
+++ composables/jwt.ts
@@ -0,0 +1,36 @@
+import type { User } from "@prisma/client";
+import { jwtVerify, SignJWT } from "jose";
+
+export const generateToken = async (data: User) => {
+  const secretKey = process.env["SECRET_KEY"];
+
+  if (!secretKey) {
+    throw createError({ statusCode: 500, message: "Erro interno" });
+  }
+
+  const secret = new TextEncoder().encode(secretKey);
+
+  const { id, name, email } = data;
+  const tokenData = { id, name, email };
+
+  const token = await new SignJWT(tokenData)
+    .setProtectedHeader({ alg: "HS256" })
+    .setExpirationTime("10s")
+    .sign(secret);
+
+  return token;
+};
+
+export const verifyToken = async (token: string) => {
+  const secretKey = process.env["SECRET_KEY"];
+
+  if (!secretKey) {
+    throw createError({ statusCode: 500, message: "Erro interno" });
+  }
+
+  const secret = new TextEncoder().encode(secretKey);
+
+  const { payload } = await jwtVerify(token, secret);
+
+  return payload;
+};

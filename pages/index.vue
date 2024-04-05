074ab712c633978255545f1b5e9c14a14b0a026e
Gabriel Santos <gabriel.santos@blitzpay.com.br>
2024-04-05 16:03:45 -0300

    start bet history feature

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,8 @@
-<script lang="ts" setup>
-const betStore = useBetStore();
-const { getActiveBets } = betStore;
-const { bets } = storeToRefs(betStore);
-
-onMounted(getActiveBets);
-</script>
+<script lang="ts" setup></script>
 
 <template>
   <div>
-    <div class="text-xl">Apostas ativas</div>
+    <div class="text-xl">Resumo</div>
     <u-divider class="my-2" />
-
-    <div class="flex gap-4">
-      <bet-card v-for="bet in bets" :key="bet.id" :bet>
-        {{ bet }}
-      </bet-card>
-    </div>
   </div>
 </template>

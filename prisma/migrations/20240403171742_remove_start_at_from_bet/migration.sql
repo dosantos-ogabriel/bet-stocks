05e9cc80c2f605c7c591c9c119a639ba0f35a1b3
Gabriel Santos <gabriel.santos@blitzpay.com.br>
2024-04-06 16:57:03 -0300

    fix migrations and add description field to bet

--- prisma/migrations/20240403171742_remove_start_at_from_bet/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `startAt` on the `Bet` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Bet" DROP COLUMN "startAt";
--- prisma/migrations/20240406191319_init/migration.sql
+++ prisma/migrations/20240406191319_init/migration.sql
@@ -3,7 +3,7 @@ CREATE TABLE "Company" (
     "id" SERIAL NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "name" TEXT NOT NULL,
-    "amount" TEXT NOT NULL,
+    "amount" DOUBLE PRECISION NOT NULL,
 
     CONSTRAINT "Company_pkey" PRIMARY KEY ("id")
 );
@@ -13,7 +13,9 @@ CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "name" TEXT NOT NULL,
-    "amount" DOUBLE PRECISION NOT NULL,
+    "amount" DOUBLE PRECISION NOT NULL DEFAULT 0,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
@@ -22,6 +24,7 @@ CREATE TABLE "User" (
 CREATE TABLE "Deposit" (
     "id" SERIAL NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "amount" DOUBLE PRECISION NOT NULL,
     "userId" INTEGER NOT NULL,
 
     CONSTRAINT "Deposit_pkey" PRIMARY KEY ("id")
@@ -31,6 +34,7 @@ CREATE TABLE "Deposit" (
 CREATE TABLE "Withdraw" (
     "id" SERIAL NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "amount" DOUBLE PRECISION NOT NULL,
     "userId" INTEGER NOT NULL,
 
     CONSTRAINT "Withdraw_pkey" PRIMARY KEY ("id")
@@ -42,14 +46,37 @@ CREATE TABLE "Bet" (
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "amount" DOUBLE PRECISION NOT NULL,
     "finalAmount" DOUBLE PRECISION NOT NULL,
-    "startAt" TIMESTAMP(3) NOT NULL,
     "finishedAt" TIMESTAMP(3),
+    "description" TEXT,
 
     CONSTRAINT "Bet_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateTable
+CREATE TABLE "BetHistory" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deviation" DOUBLE PRECISION NOT NULL,
+    "betId" INTEGER NOT NULL,
+    "companyId" INTEGER NOT NULL,
+
+    CONSTRAINT "BetHistory_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "BetHistory_betId_key" ON "BetHistory"("betId");
 
 -- AddForeignKey
 ALTER TABLE "Deposit" ADD CONSTRAINT "Deposit_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Withdraw" ADD CONSTRAINT "Withdraw_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "BetHistory" ADD CONSTRAINT "BetHistory_betId_fkey" FOREIGN KEY ("betId") REFERENCES "Bet"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "BetHistory" ADD CONSTRAINT "BetHistory_companyId_fkey" FOREIGN KEY ("companyId") REFERENCES "Company"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
